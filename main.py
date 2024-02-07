ceecc2f11b1519b6e6ec48469dea3033a33a8236
Jamison <11932076+jamisonn@users.noreply.github.com>
2024-02-07 00:09:16 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+check = float(input("How much is your check?"))
+rent = 623.00
+check -= rent
+print(check)
+busfare = 95.00
+check -= busfare
+print(check)
+results = 0
+print("Big bills done. Time for the Budget breakdown.")
+
+bills = 0.62
+print(check * bills)
+
+food = 0.12
+print(check * food)
+
+savings = 0.12
+print(check * savings)
+Investment = 0.12
+print(check * Investment)
